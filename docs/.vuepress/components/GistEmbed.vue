dcdd2dc74b3a5972b8671252f271b196b8460600
tubone24 <tubo.yyyuuu@gmail.com>
2019-09-08 10:59:50 +0900

    embed gist

---
+++ docs/.vuepress/components/GistEmbed.vue
@@ -0,0 +1,19 @@
+<template>
+    <vue-embed-gist
+            gist-id={{gistId}}
+            file={{file}}/>
+</template>
+
+<script>
+  import VueEmbedGist from 'vue-embed-gist'
+
+  export default {
+    props: {
+      gistId: String,
+      file: String
+    },
+    components: {
+      VueEmbedGist
+    }
+  }
+</script>
