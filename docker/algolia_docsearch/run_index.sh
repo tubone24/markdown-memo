18f085e532986012224eb63533431676608fcbe5
tubone <meitantei-conan@live.jp>
2021-01-11 02:14:43 +0900

    Update run_index.sh

--- docker/algolia_docsearch/run_index.sh
+++ docker/algolia_docsearch/run_index.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 echo "API_KEY=${ALGOLIA_API_KEY}" >> .env
+echo "APPLICATION_ID=${ALGOLIA_APPLICATION_ID}" >> .env
 #pipenv install
 #pipenv install -r requirements.txt
 ./docsearch run /root/docsearch-scraper/config.json
