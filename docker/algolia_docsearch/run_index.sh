26ffa67c1fc2c247b0851473c8c8c219e2e5c989
tubone24 <tubo.yyyuuu@gmail.com>
2021-01-10 23:47:12 +0900

    change CI from CircleCI to GitHub Actions

--- docker/algolia_docsearch/run_index.sh
+++ docker/algolia_docsearch/run_index.sh
@@ -2,4 +2,4 @@
 echo "API_KEY=${ALGOLIA_API_KEY}" >> .env
 #pipenv install
 #pipenv install -r requirements.txt
-python3 ./docsearch run_config config.json
+python3 ./docsearch RunConfig config.json
